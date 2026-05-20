e30c01eb0e6a492de3ca20e739423f550a0baab5
esmamol <isubuesmamol@gmail.com>
2026-05-20 14:24:51 +0300

    backend: telegram bildirim sistemi eklendi

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import requests
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
-import requests
 import os
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
@@ -12,22 +13,52 @@ CORS(app)
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+last_alert_time = 0
+
 headers = {
     "apikey": SUPABASE_KEY,
     "Authorization": f"Bearer {SUPABASE_KEY}"
 }
 
+def send_telegram(msg):
+    global last_alert_time
+
+    now = time.time()
+
+    # 60 saniyede bir uyarı göndersin, spam olmasın
+    if now - last_alert_time < 60:
+        return
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": msg
+    }
+
+    try:
+        requests.post(url, data=data)
+        last_alert_time = now
+        print("Telegram bildirimi gönderildi")
+    except Exception as e:
+        print("Telegram gönderilemedi:", e)
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/dashboard")
 def dashboard():
     return render_template("index.html")
 
+
 @app.route("/api/live")
 def live():
-
     r = requests.get(
         f"{SUPABASE_URL}/rest/v1/sensor_data?select=*&order=id.asc&limit=1",
         headers=headers
@@ -38,11 +69,24 @@ def live():
     if not data:
         return jsonify({})
 
-    return jsonify(data[0])
+    sensor = data[0]
+
+    gas = sensor.get("gas", 0)
+    flame = sensor.get("flame_detected", False)
+
+    if gas > 2000:
+        send_telegram(f"🔥 UYARI! Gaz seviyesi yüksek: {gas}")
+
+    if flame == True:
+        send_telegram("🔥 UYARI! Alev algılandı!")
+
+    return jsonify(sensor)
+
 
 @app.route("/manifest.json")
 def manifest():
     return app.send_static_file("manifest.json")
+
 
 @app.route("/api/history")
 def history():
@@ -51,6 +95,7 @@ def history():
         headers=headers
     )
     return jsonify(r.json())
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
